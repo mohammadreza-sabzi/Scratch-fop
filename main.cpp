db7aa58c6b1c9986a81c5fdc8eda06e3799300be
mohammadreza-sabzi <sabzimohammadreza1386@gmail.com>
2026-02-18 00:18:50 +0330

    useful sdl includes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include "render.h"
 #include "utils.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL2_gfx.h>
+
 using namespace std;
 
 
