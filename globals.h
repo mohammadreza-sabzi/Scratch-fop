fda3eca5e02413e61b3dec07be3fddd5cb50ed18
mohammadreza-sabzi <sabzimohammadreza1386@gmail.com>
2026-02-18 00:43:03 +0330

    useful code/ initial ver2

--- globals.h
+++ globals.h
@@ -4,5 +4,11 @@
 
 #ifndef SCRATCH_FOP_GLOBALS_H
 #define SCRATCH_FOP_GLOBALS_H
-
+#include <SDL2/SDL_pixels.h>
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 800;
+// رنگ‌های پرکاربرد (برداشته شده از تم Darkmode در main.cpp)
+const SDL_Color COLOR_BACKGROUND = {40, 44, 52, 255};
+const SDL_Color COLOR_TEXT_WHITE = {255, 255, 255, 255};
+const SDL_Color COLOR_BLOCK_MOTION = {76, 151, 255, 255}; // آبی
 #endif //SCRATCH_FOP_GLOBALS_H
